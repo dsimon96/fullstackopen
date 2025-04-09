b088b0b10790635f120e13211963aceb798f59cd
David Simon <d@vidsimon.tech>
2025-04-09 15:41:14 -0700

    Complete part 2c exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ filter, onChange }) => {
   return (
@@ -30,13 +31,17 @@ const AddPersonForm = ({ addPerson }) => {
       <div>
         <label>
           name:&nbsp;
-          <input value={newPerson.name} onChange={handleChange} />
+          <input name="name" value={newPerson.name} onChange={handleChange} />
         </label>
       </div>
       <div>
         <label>
           number:&nbsp;
-          <input value={newPerson.number} onChange={handleChange} />
+          <input
+            name="number"
+            value={newPerson.number}
+            onChange={handleChange}
+          />
         </label>
       </div>
       <button type="submit">add</button>
@@ -65,13 +70,14 @@ const Persons = ({ persons, filter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-1234567" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (newPerson) => {
     if (persons.some((person) => person.name === newPerson.name)) {
