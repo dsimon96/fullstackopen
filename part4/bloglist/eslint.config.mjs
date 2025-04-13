f42e3ba3c309d2f3f2cd0a5492a694c9169ea71c
David Simon <d@vidsimon.tech>
2025-04-13 14:14:01 -0700

    Complete exercises 4.1-4.2

---
+++ part4/bloglist/eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, "@stylistic": stylistic },
+    extends: ["js/recommended"],
+    rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/linebreak-style": ["error", "unix"],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
+    },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
